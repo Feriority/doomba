51157372ff8410117e2ce3e824601aa59282432c
Nat Thompson <natthompson27@gmail.com>
2012-06-22 14:02:41 -0700

    spabs

--- doomba.py
+++ doomba.py
@@ -7,18 +7,18 @@ ROOMBA_PORT = '/dev/tty.usbserial-A2001mZT'
 
 
 class Doomba(pyroomba.RoombaClassic):
-	def __init__(self, port=ROOMBA_PORT, **kwargs):
-		super(Doomba, self).__init__(port, **kwargs)
-		self.start()
-		#self.safe()
-		self.speed = 200
+    def __init__(self, port=ROOMBA_PORT, **kwargs):
+        super(Doomba, self).__init__(port, **kwargs)
+        self.start()
+        #self.safe()
+        self.speed = 200
         self.turn_ratio = 0.75
 
-	def left(self):
-		self.drive_direct(-self.speed, self.speed)
+    def left(self):
+        self.drive_direct(-self.speed, self.speed)
 
-	def right(self):
-		self.drive_direct(self.speed, -self.speed)
+    def right(self):
+        self.drive_direct(self.speed, -self.speed)
 
     def forward_left(self):
         left_wheel = self.speed * self.turn_ratio
@@ -32,27 +32,27 @@ class Doomba(pyroomba.RoombaClassic):
 
         self.drive_direct(left_wheel, right_wheel)
 
-	def forward(self):
-		self.drive_direct(self.speed, self.speed)
+    def forward(self):
+        self.drive_direct(self.speed, self.speed)
 
-	def back(self):
-		self.drive_direct(-self.speed, -self.speed)
+    def back(self):
+        self.drive_direct(-self.speed, -self.speed)
 
-	def stop(self):
-		self.drive_direct(0, 0)
+    def stop(self):
+        self.drive_direct(0, 0)
 
 if __name__ == '__main__':
-	doomba = Doomba()
-	doomba.forward()
-	time.sleep(1)
-	doomba.forward_left()
-	time.sleep(1)
-	doomba.forward_right()
-	time.sleep(1)
-	doomba.right()
-	time.sleep(1)
-	doomba.left()
-	time.sleep(1)
-	doomba.back()
-	time.sleep(1)
-	doomba.stop()
+    doomba = Doomba()
+    doomba.forward()
+    time.sleep(1)
+    doomba.forward_left()
+    time.sleep(1)
+    doomba.forward_right()
+    time.sleep(1)
+    doomba.right()
+    time.sleep(1)
+    doomba.left()
+    time.sleep(1)
+    doomba.back()
+    time.sleep(1)
+    doomba.stop()
--- server.py
+++ server.py
@@ -7,79 +7,80 @@ import vulcanuino
 
 
 class MockObject(object):
-	def __getattr__(self, name):
-		return lambda *args, **kwargs: None
+    def __getattr__(self, name):
+    	print name
+        return lambda *args, **kwargs: None
 
 try:
-	doomba = doomba_.Doomba()
+    doomba = doomba_.Doomba()
 except:
-	print "Warning: Roomba not connected"
-	doomba = MockObject()
+    print "Warning: Roomba not connected"
+    doomba = MockObject()
 try:
-	gun = vulcanuino.Vulcanuino()
+    gun = vulcanuino.Vulcanuino()
 except:
-	print "Warning: Gun not connected"
-	gun = MockObject()
+    print "Warning: Gun not connected"
+    gun = MockObject()
 
 
 @route('/')
 def home():
-	return static_file('controls.html', root='/Users/nat/code/doomba/control_page')
+    return static_file('controls.html', root='/Users/nat/code/doomba/control_page')
 
 @route('/reset')
 def reset_doomba():
-	global doomba
-	doomba = doomba_.Doomba()
+    global doomba
+    doomba = doomba_.Doomba()
 
 @route('/forward')
 def go_forward():
-	doomba.forward()
+    doomba.forward()
 
 @route('/back')
 def go_back():
-	doomba.back()
+    doomba.back()
 
 @route('/speed')
 def set_speed():
-	doomba.speed = int(request.query.speed) or 100
+    doomba.speed = int(request.query.speed) or 100
 
 @route('/forward_left')
 def go_forward_left():
-	doomba.forward_left()
+    doomba.forward_left()
 
 @route('/forward_right')
 def go_forward_right():
-	doomba.forward_right()
+    doomba.forward_right()
 
 @route('/left')
 def turn_left():
-	doomba.left()
+    doomba.left()
 
 @route('/right')
 def turn_right():
-	doomba.right()
+    doomba.right()
 
 @route('/custom')
 def custom_movement():
-	left = request.query.left or 0
-	right = request.query.right or 0
-	doomba.drive_direct(left, right)
+    left = request.query.left or 0
+    right = request.query.right or 0
+    doomba.drive_direct(left, right)
 
 @route('/stop')
 def stop_moving():
-	doomba.stop()
+    doomba.stop()
 
 @route('/fire')
 def shoot_dart():
-	gun.single_shot()
+    gun.single_shot()
 
 @route('/start_rampage')
 def start_rampage():
-	gun.commence_firing()
+    gun.commence_firing()
 
 @route('/end_rampage')
 def end_rampage():
-	gun.cease_fire()
+    gun.cease_fire()
 
 
 run(host='10.12.5.29', port=8080)
