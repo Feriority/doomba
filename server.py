a2a274cecd9d0c442671ef12f7d797f852d3dda1
Nat Thompson <natthompson27@gmail.com>
2012-06-21 17:36:56 -0700

    Server is better

--- server.py
+++ server.py
@@ -1,30 +1,58 @@
 from bottle import route, run
 import pyroomba
 
-ROOMBA_DEV = 'dev/tty.usbserial-A2001mZT'
+ROOMBA_PORT = '/dev/tty.usbserial-A2001mZT'
 
-roomba = pyroomba.Roomba(ROOMBA_DEV)
-roomba.start()
-roomba.safe()
+
+class Doomba(pyroomba.Roomba):
+	def __init__(self, port, **kwargs):
+		super(SimpleDoomba, self).__init__(self, port, **kwargs)
+		self.start()
+		self.safe()
+		self.speed = 100
+
+	def left(self):
+		self.drive_direct(-self.speed, self.speed)
+		
+	def right(self):
+		self.drive_direct(self.speed, -self.speed)
+
+	def forward(self):
+		self.drive_direct(self.speed, self.speed)
+
+	def stop(self):
+		self.drive_direct(0, 0)
+
+
+doomba = Doomba(ROOMBA_PORT)
 
 @route('/')
 def home():
 	return "Press up to go forward, left to go left, right to go right, back to stop, and space to fire."
 
-@route('/up/:speed')
+@route('/forward')
 def go_forward(speed=100):
-	return "Moving forward at %s" % speed
+	doomba.forward()
+
+@route('/speed=:speed')
+def set_speed(speed):
+	doomba.speed = speed
 
 @route('/left')
 def turn_left():
-	return "Turning left!"
+	doomba.left()
 
 @route('/right')
 def turn_right():
-	return "Turning right!"
+	doomba.right()
+
+@route('/stop')
+def stop_moving():
+	doomba.stop()
 
 @route('/fire')
-def turn_right():
+def fire_missles():
 	return "Fire ze missles!"
+
 
 run(host='localhost', port=8080)
